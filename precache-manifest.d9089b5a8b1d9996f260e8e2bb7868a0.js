248a1a810602bb0ec4eb253df50dfe70ba778f19
Suman Barick <codotronix@gmail.com>
2019-02-14 20:55:49 +0530

    Updates

---
+++ precache-manifest.d9089b5a8b1d9996f260e8e2bb7868a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c78f999995eeb6c91eb",
+    "url": "/static/js/main.9c78f999.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "9c78f999995eeb6c91eb",
+    "url": "/static/css/main.d31a76da.chunk.css"
+  },
+  {
+    "revision": "e14367f309dfc3c624d51f3189655c06",
+    "url": "/index.html"
+  }
+];
