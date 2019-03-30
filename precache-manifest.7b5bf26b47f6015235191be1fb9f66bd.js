f39d300bdbebe249470be1e9fd5874b8d8202d52
Suman Barick <codotronix@gmail.com>
2019-03-31 01:06:07 +0530

    Updates

---
+++ precache-manifest.7b5bf26b47f6015235191be1fb9f66bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b3be4f6cafc0ba0a031",
+    "url": "/react-shop/static/js/runtime~main.4b3be4f6.js"
+  },
+  {
+    "revision": "705e223544c6ad9de0a9",
+    "url": "/react-shop/static/js/main.705e2235.chunk.js"
+  },
+  {
+    "revision": "e520dcc5f4054ba13b69",
+    "url": "/react-shop/static/js/1.e520dcc5.chunk.js"
+  },
+  {
+    "revision": "705e223544c6ad9de0a9",
+    "url": "/react-shop/static/css/main.a80a796d.chunk.css"
+  },
+  {
+    "revision": "38f2cb9f1ead769d1bf369c9f2340d30",
+    "url": "/react-shop/index.html"
+  }
+];
